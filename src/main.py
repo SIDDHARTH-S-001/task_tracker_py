11742b72d5830799bc244c38af964eef0d60e527
Siddharth <sid2k01@gmai.com>
2025-10-31 22:18:08 +0000

    add: update method

--- src/main.py
+++ src/main.py
@@ -23,7 +23,16 @@ class TaskTrackerPy():
             print(f"Registry is empty")
         return self.tasks
 
+    def update_status(self, task, status):
+        if len(self.tasks) > 0:
+            for t in self.tasks:
+                assert t["name"] == str(task).lower(), f"The requested task {task} doesn't exist in the registry. Run 'task-cli list' to verify registry contents"
+                t["status"] = status; print(f"Task '{task}' status updated to '{status}'") if status in self.allowed_states else print(f"Invalid status {status}") 
+        print(self.tasks)
+        return self.tasks
+
 if __name__ == "__main__":
     tt = TaskTrackerPy()
     tt.add("Build Task Tracker")
-    tt.remove("Build Task")
+    # tt.remove("Build Task")
+    tt.update_status("Build Task Tracker", "complete")
