6b958bac15d84be117840fc18231da33fa716afb
Siddharth <sid2k01@gmai.com>
2025-11-01 18:59:47 +0000

    update: main - add feature to show requested list

--- src/main.py
+++ src/main.py
@@ -19,12 +19,10 @@ class TaskTrackerPy():
 
     def remove(self, task):
         self.all_tasks, self.planned, self.active, self.complete = self.memory()[0], self.memory()[1], self.memory()[2], self.memory()[3]
-        print(len(self.all_tasks))
         if len(self.all_tasks) > 0:
             for i in range(len(self.all_tasks)):
                 if self.all_tasks[i]["name"] == str(task).lower():
                     task_status = self.all_tasks[i]["status"]
-                    print(task_status)
                     self.all_tasks.pop(i)
                     if task_status=="planned":
                         for j in range(len(self.planned)):
@@ -94,20 +92,30 @@ class TaskTrackerPy():
         parser.add_argument("-a", "--add", type=str, nargs="+",  help="Add task to registry, pass 2 string arguments for taskname & description")
         parser.add_argument("-r", "--remove", type=str, nargs=1,  help="remove task from registry, pass 1 string argument for taskname")
         parser.add_argument("-u", "--update", type=str, nargs=2,  help="Update task in registry, pass 2 string arguments for taskname & new status")
-        parser.add_argument("-s", "--show", action="store_true", help="Shows all the current tasks")
+        parser.add_argument("-s", "--show", choices=["all", "planned", "active", "complete"], default=None, help="Shows all the current tasks")
         args = parser.parse_args()
-        print(args)
         if args.add:
             self.add(args.add[0], args.add[1] if len(args.add) > 1 else "")
         if args.remove:
             self.remove(args.remove[0])
         if args.update:
             self.update_status(args.update[0], args.update[1])
+        
+        current_data = self.memory()
+        all_data, planned, active, complete = current_data[0], current_data[1], current_data[2], current_data[3]
         if args.show:
-            current_data = self.memory()
-            all_data, planned, active, complete = current_data[0], current_data[1], current_data[2], current_data[3]
-            print(all_data)
-    
+            match args.show:
+                case "all":
+                    print(all_data)
+                case "planned":
+                    print(planned)
+                case "active":
+                    print(active)
+                case "complete":
+                    print(complete)
+                case _:
+                    print(all_data)
+                    
     def to_json(self, filename):
         contents = [self.all_tasks, self.planned, self.active, self.complete]
         with open(filename, mode="w", encoding="utf-8") as write_file:
